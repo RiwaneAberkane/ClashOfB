143ac1ff73af14f62d2e3a6d878d8091bd231716
beuleu <billyklotz789@gmail.com>
2023-07-25 11:27:32 +0200

    Player io join

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,36 @@
-import express, {Express} from "express";
-import http from "http";
-import {Server} from "socket.io";
-import { Socket } from "socket.io";
+import express, { Express } from "express";
+import http                 from "http";
+import { Server, Socket }   from "socket.io";
+import { Player }           from "./interfaces/Player.model";
 
-const app : Express = express();
+const app: Express = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-io.on('connection', (socket: Socket) => {
-    console.log(`user connected: ${socket.id}`);
-socket.on(`disconnect`, () => console.log(`user disconnected: ${socket.id}`)
-)});
+const players: { [key: string]: Player } = {}
+io.on("connection", (socket: Socket) => {
+  console.log(`user connected: ${ socket.id }`);
+
+  socket.on("join", (name: string) => {
+    const newPlayer: Player = {
+      id: socket.id,
+      name: name,
+      ressources: 0
+    }
+    players[socket.id] = newPlayer
+    console.log(`${ name } has joined the game`)
+    socket.emit("joined", newPlayer)
+  })
+
+  socket.on(`disconnect`, () => {
+        const player = players[socket.id]
+        if ( player ) {
+          console.log(`${ player.name } left the game`)
+          delete players[socket.id]
+        }
+      }
+  )
+});
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => console.log("listening on port " + port)
